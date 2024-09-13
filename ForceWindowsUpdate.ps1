c0052870c1186b1bfd772ec5970fdc43147e917e
denislimasan <151691897+DenisSantosLima@users.noreply.github.com>
2024-09-13 17:41:14 -0300

    Update ForceWindowsUpdate.ps1

--- ForceWindowsUpdate.ps1
+++ ForceWindowsUpdate.ps1
@@ -1,19 +1,21 @@
-# Configurações para não incluir drivers na busca
-$searcher = New-Object -ComObject Microsoft.Update.Searcher
+# Defina o KB que deseja instalar
+$kbNumber = "5005565"  # Substitua pelo número do KB desejado
 
-# Modifica a query para buscar atualizações que não estão instaladas e não incluem drivers
-$searchCriteria = "IsInstalled=0 AND Type='Software'"
+# Cria o objeto de busca de atualizações
+$updateSession = New-Object -ComObject Microsoft.Update.Session
+$updateSearcher = $updateSession.CreateUpdateSearcher()
 
-# Busca por atualizações com a query ajustada
-$searchResult = $searcher.Search($searchCriteria)
+# Busca por atualizações específicas com base no número do KB
+$searchCriteria = "IsInstalled=0 AND UpdateID LIKE '%$kbNumber%'"
+$searchResult = $updateSearcher.Search($searchCriteria)
 
-# Verifica se há atualizações disponíveis
+# Verifica se há atualizações correspondentes ao KB fornecido
 if ($searchResult.Updates.Count -eq 0) {
-    Write-Host "Não há atualizações pendentes."
+    Write-Host "Nenhuma atualização correspondente ao KB $kbNumber foi encontrada."
     exit
 }
 
-# Lista as atualizações encontradas
+# Adiciona a atualização encontrada à coleção de atualizações para instalação
 $updatesToInstall = New-Object -ComObject Microsoft.Update.UpdateColl
 foreach ($update in $searchResult.Updates) {
     Write-Host "Encontrada atualização: $($update.Title)"
@@ -22,21 +24,21 @@ foreach ($update in $searchResult.Updates) {
 
 # Instala as atualizações encontradas
 if ($updatesToInstall.Count -gt 0) {
-    Write-Host "Iniciando a instalação das atualizações..."
+    Write-Host "Iniciando o download e instalação da atualização KB$kbNumber..."
     $installer = New-Object -ComObject Microsoft.Update.Installer
     $installer.Updates = $updatesToInstall
     $installationResult = $installer.Install()
 
     # Verifica o resultado da instalação
     if ($installationResult.ResultCode -eq 2) {
-        Write-Host "Atualizações instaladas com sucesso!"
+        Write-Host "Atualização KB$kbNumber instalada com sucesso!"
     } else {
-        Write-Host "Houve problemas na instalação das atualizações. Código de resultado: $($installationResult.ResultCode)"
+        Write-Host "Houve problemas na instalação da atualização. Código de resultado: $($installationResult.ResultCode)"
     }
 
     # Suprime qualquer reinicialização solicitada
     if ($installationResult.RebootRequired -eq $true) {
-        Write-Host "Atualizações instaladas, mas reinicialização foi suprimida."
+        Write-Host "Atualização instalada, mas reinicialização foi suprimida."
     } else {
         Write-Host "Nenhuma reinicialização necessária."
     }
